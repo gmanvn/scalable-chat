7dcb2f6c206200525214b5b1843e7ca7f2e7f8d2
Tung Vu <tung@siliconprime.us>
2014-12-25 16:01:29 +0700

    add start file

---
+++ start4.sh
@@ -0,0 +1,4 @@
+export PORT=3001 && pm2 start server.coffee -n plume3001 -f
+export PORT=3002 && pm2 start server.coffee -n plume3002 -f
+export PORT=3003 && pm2 start server.coffee -n plume3003 -f
+export PORT=3004 && pm2 start server.coffee -n plume3004 -f
